2c13910756d97e9fe86a9db825e89c1ddf7c812f
Felipe Chama <felipe@moises.ai>
2025-07-22 09:40:00 -0300

    disabled featured VIP

--- src/app/components/Pricing.jsx
+++ src/app/components/Pricing.jsx
@@ -47,7 +47,7 @@ const plans = [
   },
   {
     name: "VIP",
-    featured: true,
+    featured: false,
     price: { Monthly: "$199", Annually: "$1,990" },
     description:
       "You’ve got a huge amount of assets but it’s not enough. To the moon.",
